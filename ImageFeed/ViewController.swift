fc50218a7f5dc1a7ca183842569ec440d4276990
jonykaban <nikita.fedorov124@gmail.com>
2026-05-04 09:56:04 +0300

    feat: initial project setup

---
+++ ImageFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImageFeed
+//
+//  Created by Никита Федоров on 04.05.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
