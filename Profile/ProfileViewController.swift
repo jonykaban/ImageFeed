2b0fc32a299cc409fab5737ad21b3d0753a2975a
jonykaban <nikita.fedorov124@gmail.com>
2026-05-18 10:41:42 +0300

    feat: add profile scene layout

---
+++ Profile/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  ImageFeed
+//
+//  Created by Никита Федоров on 18.05.2026.
+//
+
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    
+}
